4cb38639c193913d9ae1cfb705cb7e852f21c062
Unknown <unknown@gmail.com>
2025-04-29 14:51:02 +0500

    Добавьте файлы проекта.

---
+++ lazy loading/Models.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+public class ApplicationContext : DbContext
+{
+    public DbSet<User> Users { get; set; } = null!;
+    public DbSet<Company> Companies { get; set; } = null!;
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder
+            .UseLazyLoadingProxies()        // подключение lazy loading
+            .UseSqlite("Data Source=helloapp.db");
+    }
+}
+public class Company
+{
+    public int Id { get; set; }
+    public string? Name { get; set; }
+    public virtual List<User> Users { get; set; } = new();
+}
+
+public class User
+{
+    public int Id { get; set; }
+    public string? Name { get; set; }
+
+    public int? CompanyId { get; set; }
+    public virtual Company? Company { get; set; }
+}
